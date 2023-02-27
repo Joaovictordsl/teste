ddf5f28dcd286f3c78c65965d3f3bc9264f80634
Pedro <pedro@Romeus-Mac-mini.local>
2023-02-27 01:33:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const Datastore = require('nedb')
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '5mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    const data = request.body;
+    database.insert(data);
+    response.json({
+        status: 'success',
+    })
+});
